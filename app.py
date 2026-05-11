2dd6e7dee9962435a3d853955597bf12c0a9c189
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-05-11 21:07:44 +0300

    feat: added a function to show the menu

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import sys
 from storage import load_expenses, save_expenses
 from logic import sum_total, add_expense, list_expenses
 
+CATEGORIES = ["Ēdiens", "Transports", "Izklaide", "Komunālie maksājumi", "Veselība", "Iepirkšanās", "Cits"] 
+
+def show_menu():
+    """
+    Parāda galveno izvēlni un atgriež lietotāja izveli.
+    """
+    print("\n1) Pievienot izdevumu") 
+    print("2) Parādīt izdevumus") 
+    print("7) Iziet")
+
+    return input("\nIzvēlies darbību (1-7): ")
