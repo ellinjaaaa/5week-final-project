a4f8824612e2fe4fc4f94d0d26298661eed9db7d
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-05-11 19:38:57 +0300

    feat: added a function for adding an expense

--- logic.py
+++ logic.py
@@ -1,2 +1,30 @@
 def sum_total(expenses):
+    '''
+    Sasummē kopā pilnīgi visas izmaksas ar sum() un for cikla palīdzību.
+    '''
     return sum(expense["amount"] for expense in expenses)
+
+def add_expense(expenses, date, amount, category, description):
+    '''
+    Pievieno izdevumu (datums | izmaksas | kategorija | apraksts). Ja nekas netiek ievadīts, negatīvs izdevums vai nav skaitlis - atbilstoši
+    paziņojumi. Ja viss ok - pievieno expenses sarakstam doto vārdnīcu ar izdevuma atslēgām un vērtībām; tad atgriež, ka pievienots.    
+    '''
+
+    date=date.strip()
+    category=category.strip()
+    description=description.strip()
+
+    if not date or not amount or not category or not description:
+        return "Nevar būt tukša ievade."
+    
+    try:
+        amount=float(amount)
+    except ValueError:
+        return "Izdevumam jābūt skaitlim."
+    
+    if amount <=0:
+        return "Izdevums nevar būt negatīvs."
+    
+    expenses.append({"date":date, "amount":amount, "category":category, "description":description})
+
+    return f"Pievienots: {date} | {amount:.2f} | {category} | {description}"
