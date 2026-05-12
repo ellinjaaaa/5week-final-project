fd7a41b53b980bccee050eecebea5d25c00208b7
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-05-12 19:48:50 +0300

    feat: added a function for exporting to a csv file

---
+++ export.py
@@ -0,0 +1,23 @@
+import csv 
+
+def export_to_csv(expenses, filepath): 
+    """
+    Eksportē izdevumus CSV failā (Excel, Google Sheets).
+    """ 
+    try:
+        with open(filepath, "w", newline="", encoding="utf-8-sig") as f: 
+        writer = csv.writer(f) 
+        writer.writerow(["Datums", "Summa", "Kategorija", "Apraksts"]) 
+
+        for expense in expenses: 
+            writer.writerow([ 
+                expense["date"], 
+                f"{expense['amount']:.2f}", 
+                expense["category"], 
+                expense["description"], 
+            ]) 
+
+        return True
+    
+    except Exception:
+        return False
